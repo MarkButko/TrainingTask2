55f3ff597eb0ccba6e80250006836ed2502079a2
Mark Butko <butko.mark@gmail.com>
2018-07-10 01:13:05 +0300

    View class completed

--- task2/src/main/java/task2/View.java
+++ task2/src/main/java/task2/View.java
@@ -2,4 +2,18 @@ package task2;
 
 public class View {
 
+	public static final String EQUALS_STRING = "=";
+	public static final String SPACE_STRING = " ";
+	public static final String DASH_STRING = "-";
+
+	public static final String GREETING = "Hello. Try to guess number.";
+	public static final String NEW_RANGE = "Range: ";
+	public static final String LESS = "Our number is less.";
+	public static final String GREATER = "Our number is greater";
+	public static final String WRONG = "Wrong input!";
+	public static final String HISTORY = "History of guesses: ";
+
+	public void printlnMessage(String message) {
+		System.out.println(message);
+	}
 }
