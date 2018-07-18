60161d8b0e87ec310faeba4c2c276477b94f76da
Mark Butko <butko.mark@gmail.com>
2018-07-19 00:07:22 +0300

    PropertiesProvider incapsulated in View class

--- task3-regex/src/main/java/mark/butko/view/View.java
+++ task3-regex/src/main/java/mark/butko/view/View.java
@@ -2,12 +2,22 @@ package mark.butko.view;
 
 public class View {
 
+	private PropertiesProvider propertiesProvider;
+
 	public static final String EQUALS_STRING = "=";
 	public static final String SPACE_STRING = " ";
 	public static final String DASH_STRING = "-";
 
-	public void printlnMessage(String message) {
+	public View() throws Exception {
+		propertiesProvider = new PropertiesProvider("menu", "ru");
+	}
+
+	public static void printlnMessage(String message) {
 		System.out.println(message);
+	}
+
+	public PropertiesProvider getPropertiesProvider() {
+		return propertiesProvider;
 	}
 
 }
