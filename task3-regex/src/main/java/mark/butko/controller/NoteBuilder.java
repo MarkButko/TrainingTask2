01a03c604ced878c069ac516bf927491a4da8c4d
Mark Butko <butko.mark@gmail.com>
2018-07-22 02:21:32 +0300

    Exception handling for not unique emails added

--- task3-regex/src/main/java/mark/butko/controller/NoteBuilder.java
+++ task3-regex/src/main/java/mark/butko/controller/NoteBuilder.java
@@ -3,12 +3,14 @@ package mark.butko.controller;
 import static mark.butko.view.PropertyKeys.INPUT_EMAIL;
 import static mark.butko.view.PropertyKeys.INPUT_FIRST_NAME;
 import static mark.butko.view.PropertyKeys.INPUT_LAST_NAME;
+import static mark.butko.view.PropertyKeys.USER_ALREADY_EXISTS;
 
 import java.io.UnsupportedEncodingException;
 import java.util.Scanner;
 
 import mark.butko.model.RegexContainer;
 import mark.butko.model.entities.Note;
+import mark.butko.model.exceptions.UserAlreadyExistsException;
 import mark.butko.view.View;
 
 /**
@@ -39,21 +41,30 @@ public class NoteBuilder {
 	 * @throws UnsupportedEncodingException
 	 */
 	public Note build() {
-		String firstName;
-		String lastName;
-		String email;
+		String firstName = null;
+		String lastName = null;
+		String email = null;
 
 		RegexContainer languageConst = RegexContainer.valueOf(RegexContainer.class,
 				view.getPropertiesProvider().getLanguage().toUpperCase());
+
+		View.printlnMessage(view.getPropertiesProvider().getProperty(INPUT_EMAIL));
+		do {
+			try {
+				email = controllerUtil.requestStringThatMatches(languageConst.getEmail());
+				controllerUtil.checkUniqness(email);
+			} catch (UserAlreadyExistsException ex) {
+				email = null;
+				View.printlnMessage(ex.getEmail() + View.SPACED_DASH_STRING
+						+ view.getPropertiesProvider().getProperty(USER_ALREADY_EXISTS));
+			}
+		} while (email == null);
 
 		View.printlnMessage(view.getPropertiesProvider().getProperty(INPUT_FIRST_NAME));
 		firstName = controllerUtil.requestStringThatMatches(languageConst.getName());
 
 		View.printlnMessage(view.getPropertiesProvider().getProperty(INPUT_LAST_NAME));
 		lastName = controllerUtil.requestStringThatMatches(languageConst.getName());
-
-		View.printlnMessage(view.getPropertiesProvider().getProperty(INPUT_EMAIL));
-		email = controllerUtil.requestStringThatMatches(languageConst.getEmail());
 
 		note.setFirstName(firstName);
 		note.setLastName(lastName);
