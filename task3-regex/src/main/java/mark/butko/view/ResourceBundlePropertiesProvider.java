64a54e030c73c5fbbf4e41325ae345c03212c542
Mark Butko <butko.mark@gmail.com>
2018-07-22 00:28:30 +0300

    ResourceBundle added as propertyProvider for i18n purposes.
    PropertyProvider constructor call stubbed with new property
    file"stub_ru.properties".

---
+++ task3-regex/src/main/java/mark/butko/view/ResourceBundlePropertiesProvider.java
@@ -0,0 +1,51 @@
+package mark.butko.view;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * Class that was created to switch from using plain Properies class to
+ * ResourceBundle. This replacement provides better i18n opportunities.
+ * PropertiesProvider should be used as interface.
+ * 
+ * @author Butko Mark
+ *
+ */
+public class ResourceBundlePropertiesProvider extends PropertiesProvider {
+
+	private ResourceBundle resourceBundle;
+	private Locale locale;
+
+	public ResourceBundlePropertiesProvider(ResourceBundle resourceBundle, Locale locale) throws Exception {
+		super("stub");
+		this.resourceBundle = resourceBundle;
+		this.locale = locale;
+	}
+
+	@Override
+	public String getProperty(String key) {
+		return resourceBundle.getString(key);
+	}
+
+	@Override
+	public String getLanguage() {
+		return locale.getLanguage();
+	}
+
+	public ResourceBundle getResourceBundle() {
+		return resourceBundle;
+	}
+
+	public void setResourceBundle(ResourceBundle resourceBundle) {
+		this.resourceBundle = resourceBundle;
+	}
+
+	public Locale getLocale() {
+		return locale;
+	}
+
+	public void setLocale(Locale locale) {
+		this.locale = locale;
+	}
+
+}
