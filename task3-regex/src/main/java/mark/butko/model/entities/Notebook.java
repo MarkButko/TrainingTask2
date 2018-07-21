175ea6cb44a0e8d389fd186d28ff00834a194391
Mark Butko <butko.mark@gmail.com>
2018-07-22 02:14:23 +0300

    Test Note object added - db imitation

--- task3-regex/src/main/java/mark/butko/model/entities/Notebook.java
+++ task3-regex/src/main/java/mark/butko/model/entities/Notebook.java
@@ -1,7 +1,10 @@
 package mark.butko.model.entities;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import mark.butko.model.NoteConstructor;
 
 /**
  * Note objects container
@@ -13,26 +16,34 @@ public class Notebook {
 	/**
 	 * List of Note objects
 	 */
-	private List<Note> notes = new ArrayList<Note>();
+	private static List<Note> notes = new ArrayList<Note>(
+			Arrays.asList(new NoteConstructor().withEmail("already.existed@e.mail").build()));
 
 	/**
 	 * Adds new Note object to list
 	 */
-	public void addNote(Note note) {
+	public static void addNote(Note note) {
 		notes.add(note);
 	}
 
 	/**
 	 * @return Note object at index position in list
 	 */
-	public Note getNote(int index) {
+	public static Note getNote(int index) {
 		return notes.get(index);
 	}
 
 	/**
 	 * @return current number of Note objects in list
 	 */
-	public int size() {
+	public static int size() {
 		return notes.size();
+	}
+
+	/**
+	 * @return list of stored notes
+	 */
+	public static List<Note> getNotesList() {
+		return notes;
 	}
 }
