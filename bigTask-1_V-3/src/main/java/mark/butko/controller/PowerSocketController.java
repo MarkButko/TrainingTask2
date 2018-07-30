d2892726e9d2b4d8fdc396e9bb03b86ca1e60a42
Mark Butko <butko.mark@gmail.com>
2018-07-31 01:43:49 +0300

    One of controller classes that interacts with db

--- bigTask-1_V-3/src/main/java/mark/butko/controller/PowerSocketController.java
+++ bigTask-1_V-3/src/main/java/mark/butko/controller/PowerSocketController.java
@@ -1,18 +1,37 @@
 package mark.butko.controller;
 
 import java.util.Comparator;
+import java.util.Set;
 import java.util.TreeSet;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import mark.butko.controller.filters.ColorFilterChain;
+import mark.butko.controller.filters.FilterChain;
+import mark.butko.controller.filters.PowerFilterChain;
+import mark.butko.controller.filters.WeightFilterChain;
+import mark.butko.controller.filters.WireLengthFilterChain;
 import mark.butko.data.DataBaseEmulator;
+import mark.butko.model.ApplianceFeatures;
 import mark.butko.model.entities.consumers.ElectricalAppliance;
 import mark.butko.model.entities.providers.PowerSocket;
 
 public class PowerSocketController {
 
 	private static PowerSocket powerSocket = new PowerSocket();
+	private static FilterChain filterChain;
+
+	static {
+		filterChain = new ColorFilterChain();
+		FilterChain wireLengthFilterChain = new WireLengthFilterChain();
+		FilterChain powerFilterChain = new PowerFilterChain();
+		FilterChain weightFilterChain = new WeightFilterChain();
+
+		filterChain.setNextChain(wireLengthFilterChain);
+		wireLengthFilterChain.setNextChain(powerFilterChain);
+		powerFilterChain.setNextChain(weightFilterChain);
+	}
 
 	public static void turnOnDevice(HttpServletRequest request, HttpServletResponse response) {
 		Long idLong = Long.parseLong(request.getParameter("id"));
@@ -21,36 +40,27 @@ public class PowerSocketController {
 
 	public static void countPowerConsumption(HttpServletRequest request, HttpServletResponse response) {
 		int totalPowerConsumption = powerSocket.powerConsumption();
-		request
-//		.getSession()
-				.setAttribute("totalPowerConsumption", totalPowerConsumption);
+		request.getSession().setAttribute("totalPowerConsumption", totalPowerConsumption);
 	}
 
-	public static void sort(HttpServletRequest request, HttpServletResponse response) {
-		String sortType = request.getParameter("sort_type");
-		Comparator<ElectricalAppliance> comparator = SortType.valueOf(sortType.toUpperCase()).getComparator();
+	public static void filter(HttpServletRequest request, HttpServletResponse response) {
 
-		TreeSet<ElectricalAppliance> sortedAppliances = new TreeSet<>(comparator);
-		sortedAppliances.addAll(DataBaseEmulator.getData());
+		Set<ElectricalAppliance> appliances = DataBaseEmulator.getData();
+		Set<ElectricalAppliance> filteredAppliances = filterChain.getFiltered(appliances, request);
 
-		request
-//		.getSession()
-				.setAttribute("appliances", sortedAppliances);
-	}
-
-	enum SortType {
-		POWER {
-			public Comparator<ElectricalAppliance> getComparator() {
-				return new ElectricalAppliance.PowerComparator().thenComparing(new ElectricalAppliance.IDComparator());
-			}
-		},
-		COLOR {
-			public Comparator<ElectricalAppliance> getComparator() {
-				return new ElectricalAppliance.ColorComparator().thenComparing(new ElectricalAppliance.IDComparator());
-			}
-		};
-		public Comparator<ElectricalAppliance> getComparator() {
-			return null;
+		String sortType = (String) request.getSession().getAttribute("sort-type");
+		if (sortType == null) {
+			sortType = request.getParameter("sort-type");
 		}
+		Comparator<ElectricalAppliance> comparator;
+		try {
+			comparator = ApplianceFeatures.valueOf(sortType.toUpperCase()).getComparator();
+		} catch (Exception ex) {
+			comparator = ApplianceFeatures.POWER.getComparator();
+		}
+		TreeSet<ElectricalAppliance> sortedAppliances = new TreeSet<>(comparator);
+		sortedAppliances.addAll(filteredAppliances);
+
+		request.getSession().setAttribute("appliances", sortedAppliances);
 	}
 }
