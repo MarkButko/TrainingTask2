f2512ca0063ca270f0083d25a4f516b6a2154657
Mark Butko <butko.mark@gmail.com>
2018-08-02 03:28:13 +0300

    utf-8 encoding filter

---
+++ bigTask-1_V-3/src/main/java/mark/butko/controller/filters/EncodingFilter.java
@@ -0,0 +1,35 @@
+package mark.butko.controller.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class EncodingFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
+		request.setCharacterEncoding("UTF-8");
+		System.out.println("Filter");
+		chain.doFilter(request, response);
+
+	}
+
+	@Override
+	public void destroy() {
+
+	}
+
+}
